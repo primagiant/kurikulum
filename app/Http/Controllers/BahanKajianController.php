51a79e68b2c7d4e580e651392818ed05f2326b56
ptsuardana01 <ptsuardana01@gmail.com>
2023-07-07 09:50:51 +0800

    CR Bahan Kajian

--- app/Http/Controllers/BahanKajianController.php
+++ app/Http/Controllers/BahanKajianController.php
@@ -22,4 +22,39 @@ class BahanKajianController extends Controller
             'filters' => $request->only(["search"]),
         ]);
     }
+
+    public function create()
+    {
+        return Inertia::render('Main/KoorProdi/Feature/BahanKajian/FormAdd');
+    }
+
+    public function store(Request $request)
+    {
+        $user = auth()->user();
+
+        //Validate
+        $validatedData = $request->validate([
+            "kode_bk" => "required",
+            "nama_bk" => "required",
+            "deskripsi_bk" => "required",
+            "bobot_min" => "required",
+            "bobot_max" => "required",
+        ]);
+
+        // Save
+        $cpl = BahanKajian::create([
+            'id_prodi' => $user->prodi,
+            'kode_bk' => $request->input('kode_bk'),
+            'nama_bk' => $request->input('nama_bk'),
+            'deskripsi_bk' => $request->input('deskripsi_bk'),
+            'bobot_min' => $request->input('bobot_min'),
+            'bobot_max' => $request->input('bobot_max'),
+        ]);
+
+        // Redirect
+        return to_route('bahan.kajian.index')->with("msg", [
+            "type" => "success", // success | error | warning | info | question
+            "text" => "Created Success"
+        ]);
+    }
 }
